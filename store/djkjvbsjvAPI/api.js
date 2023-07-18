3fb97e81c928ec2ec9c48832fa58d7cf4855a234
Crowdbotics <engineering@crowdbotics.com>
2023-07-18 15:03:55 +0000

    Update store.

--- store/djkjvbsjvAPI/api.js
+++ store/djkjvbsjvAPI/api.js
@@ -1,96 +1,97 @@
-import axios from "axios";
+import axios from "axios"
 const djkjvbsjvAPI = axios.create({
   baseURL: "https://djkjvbsjv-10.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return djkjvbsjvAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return djkjvbsjvAPI.post(`/api/v1/login/`, payload);
+  return djkjvbsjvAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return djkjvbsjvAPI.post(`/api/v1/signup/`, payload);
+  return djkjvbsjvAPI.post(`/api/v1/signup/`, payload)
 }
-
 function modules_files_uploads_list(payload) {
-  return djkjvbsjvAPI.get(`/modules/files/uploads/`);
+  return djkjvbsjvAPI.get(`/modules/files/uploads/`)
 }
-
 function modules_files_uploads_create(payload) {
-  return djkjvbsjvAPI.post(`/modules/files/uploads/`, payload);
+  return djkjvbsjvAPI.post(`/modules/files/uploads/`, payload)
 }
-
 function modules_files_uploads_retrieve(payload) {
-  return djkjvbsjvAPI.get(`/modules/files/uploads/${payload.id}/`);
+  return djkjvbsjvAPI.get(`/modules/files/uploads/${payload.id}/`)
 }
-
 function modules_files_uploads_update(payload) {
-  return djkjvbsjvAPI.put(`/modules/files/uploads/${payload.id}/`, payload);
+  return djkjvbsjvAPI.put(`/modules/files/uploads/${payload.id}/`, payload)
 }
-
 function modules_files_uploads_partial_update(payload) {
-  return djkjvbsjvAPI.patch(`/modules/files/uploads/${payload.id}/`, payload);
+  return djkjvbsjvAPI.patch(`/modules/files/uploads/${payload.id}/`, payload)
 }
-
 function modules_files_uploads_destroy(payload) {
-  return djkjvbsjvAPI.delete(`/modules/files/uploads/${payload.id}/`);
+  return djkjvbsjvAPI.delete(`/modules/files/uploads/${payload.id}/`)
 }
-
+function modules_social_auth_apple_connect_create(payload) {
+  return djkjvbsjvAPI.post(`/modules/social-auth/apple/connect/`, payload)
+}
+function modules_social_auth_apple_login_create(payload) {
+  return djkjvbsjvAPI.post(`/modules/social-auth/apple/login/`, payload)
+}
+function modules_social_auth_facebook_connect_create(payload) {
+  return djkjvbsjvAPI.post(`/modules/social-auth/facebook/connect/`, payload)
+}
+function modules_social_auth_facebook_login_create(payload) {
+  return djkjvbsjvAPI.post(`/modules/social-auth/facebook/login/`, payload)
+}
+function modules_social_auth_google_connect_create(payload) {
+  return djkjvbsjvAPI.post(`/modules/social-auth/google/connect/`, payload)
+}
+function modules_social_auth_google_login_create(payload) {
+  return djkjvbsjvAPI.post(`/modules/social-auth/google/login/`, payload)
+}
+function modules_social_auth_socialaccounts_list(payload) {
+  return djkjvbsjvAPI.get(`/modules/social-auth/socialaccounts/`)
+}
+function modules_social_auth_socialaccounts_disconnect_create(payload) {
+  return djkjvbsjvAPI.post(
+    `/modules/social-auth/socialaccounts/${payload.id}/disconnect/`,
+    payload
+  )
+}
 function rest_auth_login_create(payload) {
-  return djkjvbsjvAPI.post(`/rest-auth/login/`, payload);
+  return djkjvbsjvAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return djkjvbsjvAPI.get(`/rest-auth/logout/`);
+  return djkjvbsjvAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return djkjvbsjvAPI.post(`/rest-auth/logout/`);
+  return djkjvbsjvAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return djkjvbsjvAPI.post(`/rest-auth/password/change/`, payload);
+  return djkjvbsjvAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return djkjvbsjvAPI.post(`/rest-auth/password/reset/`, payload);
+  return djkjvbsjvAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return djkjvbsjvAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return djkjvbsjvAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return djkjvbsjvAPI.post(`/rest-auth/registration/`, payload);
+  return djkjvbsjvAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return djkjvbsjvAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return djkjvbsjvAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return djkjvbsjvAPI.get(`/rest-auth/user/`);
+  return djkjvbsjvAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return djkjvbsjvAPI.put(`/rest-auth/user/`, payload);
+  return djkjvbsjvAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return djkjvbsjvAPI.patch(`/rest-auth/user/`, payload);
+  return djkjvbsjvAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -101,6 +102,14 @@ export const apiService = {
   modules_files_uploads_update,
   modules_files_uploads_partial_update,
   modules_files_uploads_destroy,
+  modules_social_auth_apple_connect_create,
+  modules_social_auth_apple_login_create,
+  modules_social_auth_facebook_connect_create,
+  modules_social_auth_facebook_login_create,
+  modules_social_auth_google_connect_create,
+  modules_social_auth_google_login_create,
+  modules_social_auth_socialaccounts_list,
+  modules_social_auth_socialaccounts_disconnect_create,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
@@ -112,4 +121,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
